fbcb504e442ae87ef6e01a199f2c29a7d87f3222
Kolozsvári Barnabás <kolozsvari.barnabas2006@tanulo.boronkay.hu>
2024-11-06 12:02:44 +0100

    tabcontrol v1

--- Projektmunka_24_I/Form1.cs
+++ Projektmunka_24_I/Form1.cs
@@ -12,6 +12,11 @@ namespace Projektmunka_24_I
 {
     public partial class Form1 : Form
     {
+        private TabPage tabPage1,tabPage2, tabPage3;
+        private TabControl tabControl1;
+
+        Label label1, label2, label3;
+
         public Form1()
         {
             InitializeComponent();
@@ -19,7 +24,55 @@ namespace Projektmunka_24_I
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            Console.WriteLine("Hello world!");
+            this.tabControl1 = new TabControl()
+            {
+                Location = new Point(0, 0),
+                Size = new Size(ClientSize.Width, ClientSize.Height),
+                Appearance = TabAppearance.FlatButtons,
+                Dock = DockStyle.Fill,
+                Padding = new Point(0, 0),
+
+            };
+
+            this.tabPage1 = new TabPage()
+            {
+                Text = "Fejben 21",
+                BackColor = Color.Red,
+
+            };
+            this.tabPage2 = new TabPage()
+            {
+                Text = "Tic-Tac-Toe",
+                BackColor = Color.Green,
+
+            };
+            this.tabPage3 = new TabPage()
+            {
+                Text = "Nim játék",
+                BackColor = Color.Blue,
+
+            };
+
+            label1 = new Label()
+            {
+                Parent = tabPage1,
+                Text = "label1",
+            };
+            label2 = new Label()
+            {
+                Parent = tabPage2,
+                Text = "label2",
+            };
+
+            this.tabControl1.TabPages.Add(tabPage1);
+            this.tabControl1.TabPages.Add(tabPage2);
+            this.tabControl1.TabPages.Add(tabPage3);
+
+
+
+            this.Controls.Add(tabControl1);
+
+            this.ClientSize = new Size(500, 500);
         }
     }
 }
